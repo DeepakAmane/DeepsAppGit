dadbdea4e8d813b5907d95834e8b1b96d597ac71
DeepakAmane <deepaksubamane@gmail.com>
2022-07-18 17:25:26 +0530

    comment added change (2)

--- app/src/main/java/com/example/deepsappgit/MainActivity.kt
+++ app/src/main/java/com/example/deepsappgit/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.deepsappgit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//  comment added (2)
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
